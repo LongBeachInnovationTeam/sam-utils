32c09b9d88da21c5f46094d881aeba600734fa15
Alex Chavez <alex@alexchavez.net>
2015-10-28 19:26:58 -0700

    Duplicate addresses are not reverse geocoded nor added to the locations table

--- sam_geocoding.py
+++ sam_geocoding.py
@@ -29,23 +29,57 @@ geolocator = GoogleV3()
 # Initialize address parser
 address_parser = AddressParser()
 
+# Fetch the list of contacts to search
+sam_addresses_cursor = db.contacts.find({"address": {"$ne": ""}}, {"address": 1, "_id": 0}).sort("address")
+tmp_list = list(sam_addresses_cursor)
+sam_addresses = list()
+for a in tmp_list:
+	if 'address' in a:
+		sam_addresses.append(a['address'])
+
+# Fetch the set of locations to search and compare to found addresses
+locations_set = set()
+for location in db.locations.find({"address": {"$ne": ""}}, {"address": 1, "_id": 0}).sort("address"):
+  # If value has not been encountered yet, add it to the list
+  if location['address'] not in locations_set:
+		locations_set.add(location['address'])
+
+def remove_duplicates(values):
+  output = []
+  seen = set()
+  for value in values:
+    # If value has not been encountered yet, add it to both list and set
+    if value not in seen:
+      output.append(value)
+      seen.add(value)
+  return output
+
+def get_unique_address_list():
+	addresses = list()
+	for a in sam_addresses:
+		if a not in locations_set:
+			addresses.append(a)
+	return remove_duplicates(addresses)
+
 def get_geocoded_list():
-	# Create a list of all addresses
+	# Create a unique list of all addresses
 	addresses = get_unique_address_list()
-	# Prepare a rate limited queue to execute the geocode function
-	queue = RateLimitedQueue(
-		addresses,
-		processing_function = geolocator.geocode,
-		rate_limits = [rate_limit]
-	)
-	# Grabs the geocoded locations, but doesn't process
-	# more than ten per second
-	geocoded_locations = queue.process()
-	# Prepare our location document for DB insert
-	addresses_list = list()
-	for i in range(len(addresses)):
-		address = addresses[i]
-		if is_unique_address(address):
+	# If unique addresses are found, find geolocation and insert
+	# into the locations table
+	if addresses > 0:
+		# Prepare a rate limited queue to execute the geocode function
+		queue = RateLimitedQueue(
+			addresses,
+			processing_function = geolocator.geocode,
+			rate_limits = [rate_limit]
+		)
+		# Grabs the geocoded locations, but doesn't process
+		# more than ten per second
+		geocoded_locations = queue.process()
+		# Prepare our location document for DB insert
+		addresses_list = list()
+		for i in range(len(addresses)):
+			address = addresses[i]
 			location = geocoded_locations[i]
 			if location is not None:
 				logging.info("Found location info for '%s'", address)
@@ -55,22 +89,9 @@ def get_geocoded_list():
 					"latitude": location.latitude,
 					"zipcode": address_parser.parse_address(location.address).zip
 				})
-	return addresses_list
-
-def is_unique_address(address):
-	for loc in db.locations.find():
-		if loc['address'] is not address:
-			return False
-	return True
-
-def get_unique_address_list():
-	addresses = list()
-	for c in db.contacts.find():
-		if 'address' in c:
-			a = c['address']
-			if a not in addresses:
-				addresses.append(a)
-	return addresses
+		return addresses_list
+	else:
+		return list()
 
 # Insert into db
 addresses_list = get_geocoded_list()
